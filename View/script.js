7a8b32e1bb5accad030247a39ebf44ee5d5b2ac5
Restyled.io <commits@restyled.io>
2022-07-11 15:20:08 +0000

    Restyled by clang-format

--- View/script.js
+++ View/script.js
@@ -1,20 +1,19 @@
 const product_list = document.querySelector("#product_list");
 const product = document.querySelector('#product');
 
-$(".navbar .nav-link").on("click", function(){
-    $(".navbar").find(".active").removeClass("active");
-    $(this).addClass("active");
- });
+$(".navbar .nav-link").on("click", function() {
+  $(".navbar").find(".active").removeClass("active");
+  $(this).addClass("active");
+});
 
 product_list.innerHTML = null;
- for (let index = 0; index < 5; index++) {
-    const col = document.createElement("div");
-    col.classList.add("col-12");
-    col.classList.add("col-lg-3");
-    col.classList.add("col-md-6");
-    col.classList.add("col-sm-12");
-    col.classList.add("p-5");
-    col.innerHTML = product.innerHTML;
-    product_list.appendChild(col);
-    
- }
+for (let index = 0; index < 5; index++) {
+  const col = document.createElement("div");
+  col.classList.add("col-12");
+  col.classList.add("col-lg-3");
+  col.classList.add("col-md-6");
+  col.classList.add("col-sm-12");
+  col.classList.add("p-5");
+  col.innerHTML = product.innerHTML;
+  product_list.appendChild(col);
+}
