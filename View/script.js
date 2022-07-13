60b4ebd6ff2becf9fa775dddfa4765dfba913e67
Restyled.io <commits@restyled.io>
2022-07-13 14:26:03 +0000

    Restyled by prettier

--- View/script.js
+++ View/script.js
@@ -1,13 +1,13 @@
 let currentAuthScreen = "Sign in";
 // Navbar navigation
-$(".navbar .nav-link").on("click", function() {
+$(".navbar .nav-link").on("click", function () {
   $(".navbar").find(".active").removeClass("active");
   $(this).addClass("active");
 });
 
 // Account modal Navigation
-$(document).ready(function() {
-  $(".nav .nav-link").on("click", function() {
+$(document).ready(function () {
+  $(".nav .nav-link").on("click", function () {
     $(".nav").find(".active").removeClass("active");
     $(this).addClass("active");
     currentAuthScreen = $(this).text();
@@ -18,7 +18,7 @@ $(document).ready(function() {
 changeText();
 
 function changeText() {
-  $(document).ready(function() {
+  $(document).ready(function () {
     $(".modal-footer .save").text(currentAuthScreen + " now");
   });
 }
@@ -31,7 +31,9 @@ function loadNow(opacity) {
     displayContent();
   } else {
     loader.style.opacity = opacity;
-    window.setTimeout(function() { loadNow(opacity - 0.05); }, 50);
+    window.setTimeout(function () {
+      loadNow(opacity - 0.05);
+    }, 50);
   }
 }
 
@@ -40,7 +42,7 @@ function displayContent() {
   document.getElementById("content").style.display = "block";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   loader = document.getElementById("loader");
   loadNow(1);
 });
@@ -50,8 +52,8 @@ function readURL(input) {
   if (input.files && input.files[0]) {
     var reader = new FileReader();
 
-    reader.onload = function(e) {
-      $('#avatar').attr('src', e.target.result).width(150).height(150);
+    reader.onload = function (e) {
+      $("#avatar").attr("src", e.target.result).width(150).height(150);
     };
 
     reader.readAsDataURL(input.files[0]);
