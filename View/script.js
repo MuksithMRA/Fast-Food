8cf872607b432c3f0dd8a60481050366326678d8
Restyled.io <commits@restyled.io>
2022-07-13 06:06:17 +0000

    Restyled by clang-format

--- View/script.js
+++ View/script.js
@@ -1,36 +1,28 @@
 
-//Navbar navigation
-$(".navbar .nav-link").on("click", function(){
-    $(".navbar").find(".active").removeClass("active");
-    $(this).addClass("active");
- });
+// Navbar navigation
+$(".navbar .nav-link").on("click", function() {
+  $(".navbar").find(".active").removeClass("active");
+  $(this).addClass("active");
+});
 
-
- //Adding preloader
- var loader = document.querySelector("#loader");
+// Adding preloader
+var loader = document.querySelector("#loader");
 
 function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.05);
-        }, 50);
-    }
+  if (opacity <= 0) {
+    displayContent();
+  } else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function() { loadNow(opacity - 0.05); }, 50);
+  }
 }
 
 function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
+  loader.style.display = 'none';
+  document.getElementById('content').style.display = 'block';
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
+  loader = document.getElementById('loader');
+  loadNow(1);
 });
-
-
-
-
-
