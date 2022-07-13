bb85555ca5ff4d3c5131800bfb4d9f8677033ecb
MuksithMRA <muksithmra@gmail.com>
2022-07-13 15:10:46 +0530

    documet ready in js

--- View/script.js
+++ View/script.js
@@ -1,47 +1,44 @@
-let  currentAuthScreen = "Login";
+let currentAuthScreen = "Login";
 //Navbar navigation
-$(".navbar .nav-link").on("click", function(){
-    $(".navbar").find(".active").removeClass("active");
-    $(this).addClass("active");
- });
+$(".navbar .nav-link").on("click", function () {
+  $(".navbar").find(".active").removeClass("active");
+  $(this).addClass("active");
+});
 
 //Account modal Navigation
-$(".nav .nav-link").on("click", function(){
-    
+$(document).ready(function () {
+  $(".nav .nav-link").on("click", function () {
     $(".nav").find(".active").removeClass("active");
     $(this).addClass("active");
-    currentAuthScreen =  $(this).text();
-    $(".modal-footer .save").text(currentAuthScreen+" now");
- });
+    currentAuthScreen = $(this).text();
+    $(".modal-footer .save").text(currentAuthScreen + " now");
+  });
+});
 
-$(".modal-footer .save").text(currentAuthScreen+" now");
+$(document).ready(function () {
+  $(".modal-footer .save").text(currentAuthScreen + " now");
+});
 
-
- //Adding preloader
- var loader = document.querySelector("#loader");
+//Adding preloader
+var loader = document.querySelector("#loader");
 
 function loadNow(opacity) {
-    if (opacity <= 0) {
-        displayContent();
-    } else {
-        loader.style.opacity = opacity;
-        window.setTimeout(function() {
-            loadNow(opacity - 0.05);
-        }, 50);
-    }
+  if (opacity <= 0) {
+    displayContent();
+  } else {
+    loader.style.opacity = opacity;
+    window.setTimeout(function () {
+      loadNow(opacity - 0.05);
+    }, 50);
+  }
 }
 
 function displayContent() {
-    loader.style.display = 'none';
-    document.getElementById('content').style.display = 'block';
+  loader.style.display = "none";
+  document.getElementById("content").style.display = "block";
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    loader = document.getElementById('loader');
-    loadNow(1);
+document.addEventListener("DOMContentLoaded", function () {
+  loader = document.getElementById("loader");
+  loadNow(1);
 });
-
-
-
-
-
