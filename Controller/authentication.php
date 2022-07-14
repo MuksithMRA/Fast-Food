426549d2c7df0dada0761db917ac4e2182d34c29
Restyled.io <commits@restyled.io>
2022-07-14 08:51:56 +0000

    Restyled by php-cs-fixer

--- Controller/authentication.php
+++ Controller/authentication.php
@@ -1,4 +1,5 @@
 <?php
+
     $avatar = $_POST["avatar"];
     $first_name = $_POST["fname"];
     $last_name = $_POST["lname"];
@@ -7,7 +8,3 @@
     $email = $_POST["email"];
     $password = $_POST["password"];
     $confirm_password = $_POST["confirmPassword"];
-
-    
-
-?>
--- Model/Customer.php
+++ Model/Customer.php
@@ -1,6 +1,7 @@
 <?php
 
-    Class Customer{
+    class Customer
+    {
         private $id;
         private $first_name;
         private $last_name;
@@ -8,7 +9,7 @@
         private $phone_number;
         private $auth_id;
 
-        public function __construct($id,$first_name,$last_name,$address,$phone_number,$auth_id)
+        public function __construct($id, $first_name, $last_name, $address, $phone_number, $auth_id)
         {
             $this->id = $id;
             $this->first_name = $first_name;
@@ -18,100 +19,112 @@
             $this->auth_id = $auth_id;
         }
 
-        
-    	/**
-	 * @return mixed
-	 */
-	function getId() {
-		return $this->id;
-	}
-	
-	/**
-	 * @param mixed $id 
-	 * @return Customer
-	 */
-	function setId($id): self {
-		$this->id = $id;
-		return $this;
-	}
-	
-	/**
-	 * @return mixed
-	 */
-	function getFirst_name() {
-		return $this->first_name;
-	}
-	
-	/**
-	 * @param mixed $first_name 
-	 * @return Customer
-	 */
-	function setFirst_name($first_name): self {
-		$this->first_name = $first_name;
-		return $this;
-	}
-	
-	/**
-	 * @return mixed
-	 */
-	function getLast_name() {
-		return $this->last_name;
-	}
-	
-	/**
-	 * @param mixed $last_name 
-	 * @return Customer
-	 */
-	function setLast_name($last_name): self {
-		$this->last_name = $last_name;
-		return $this;
-	}
-	
-	/**
-	 * @return mixed
-	 */
-	function getAddress() {
-		return $this->address;
-	}
-	
-	/**
-	 * @param mixed $address 
-	 * @return Customer
-	 */
-	function setAddress($address): self {
-		$this->address = $address;
-		return $this;
-	}
-	
-	/**
-	 * @return mixed
-	 */
-	function getPhone_number() {
-		return $this->phone_number;
-	}
-	
-	/**
-	 * @param mixed $phone_number 
-	 * @return Customer
-	 */
-	function setPhone_number($phone_number): self {
-		$this->phone_number = $phone_number;
-		return $this;
-	}
-	
-	/**
-	 * @return mixed
-	 */
-	function getAuth_id() {
-		return $this->auth_id;
-	}
-	
-	/**
-	 * @param mixed $auth_id 
-	 * @return Customer
-	 */
-	function setAuth_id($auth_id): self {
-		$this->auth_id = $auth_id;
-		return $this;
-	}
-}
+
+        /**
+     * @return mixed
+     */
+        public function getId()
+        {
+            return $this->id;
+        }
+
+        /**
+         * @param mixed $id
+         * @return Customer
+         */
+        public function setId($id): self
+        {
+            $this->id = $id;
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getFirst_name()
+        {
+            return $this->first_name;
+        }
+
+        /**
+         * @param mixed $first_name
+         * @return Customer
+         */
+        public function setFirst_name($first_name): self
+        {
+            $this->first_name = $first_name;
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getLast_name()
+        {
+            return $this->last_name;
+        }
+
+        /**
+         * @param mixed $last_name
+         * @return Customer
+         */
+        public function setLast_name($last_name): self
+        {
+            $this->last_name = $last_name;
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getAddress()
+        {
+            return $this->address;
+        }
+
+        /**
+         * @param mixed $address
+         * @return Customer
+         */
+        public function setAddress($address): self
+        {
+            $this->address = $address;
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getPhone_number()
+        {
+            return $this->phone_number;
+        }
+
+        /**
+         * @param mixed $phone_number
+         * @return Customer
+         */
+        public function setPhone_number($phone_number): self
+        {
+            $this->phone_number = $phone_number;
+            return $this;
+        }
+
+        /**
+         * @return mixed
+         */
+        public function getAuth_id()
+        {
+            return $this->auth_id;
+        }
+
+        /**
+         * @param mixed $auth_id
+         * @return Customer
+         */
+        public function setAuth_id($auth_id): self
+        {
+            $this->auth_id = $auth_id;
+            return $this;
+        }
+    }
