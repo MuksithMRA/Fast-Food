4869c80155a882f4524d17caa8554afa8d765768
MuksithMRA <muksithmra@gmail.com>
2022-07-22 22:24:08 +0530

    added about us

---
+++ View/About Us/about_us.php
@@ -0,0 +1,118 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="shortcut icon" href="/Images/logo.png" type="image/x-icon" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" />
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/09789629f4.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./about_us.css">
+    <title>About Us</title>
+</head>
+
+<body>
+    <div class="fluid-container w-100 d-flex align-items-center justify-content-center" style="height: 15vh;background-color:#FF9F1A">
+        <h2 class="text-white text-center"><i class="fa fa-group" aria-hidden="true"></i>&nbsp; About Us</h2>
+    </div>
+    <br>
+    <div class="row m-3">
+        <div class="h3">Developer Team</div>
+        <div class="underline bg-warning mx-3" style="height: 4px; width:2rem"></div>
+    </div>
+    <div class="row m-3">
+        <div class="col-12 col-lg-4 col-md-6 p-3">
+            <div class="card" style="width:18rem;">
+                <img src="/Images/Muksith.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Abdul Muksith (TL)</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">CODSE211F-051</h6>
+                    <p class="card-text">
+                        Home Page UI With Backend Implementation
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 p-3">
+            <div class="card" style="width:18rem;">
+                <img src="/Images/Menu.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Menuka Ranasinghe</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">CODSE211F-052</h6>
+                    <p class="card-text">
+                        Checkout UI With Backend Implementation
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 p-3">
+            <div class="card" style="width:18rem;">
+                <img src="/Images/aqeel.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Aqeel Husny</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">CODSE211F-032</h6>
+                    <p class="card-text">
+                        Authentication UI With Backend Implementation
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-lg-4 col-md-6 p-3">
+            <div class="card" style="width:18rem;">
+                <img src="/Images/Tharindu.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Tharindu Prasad</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">CODSE211F-001</h6>
+                    <p class="card-text">
+                        Cart UI With Backend Implementation
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 col-lg-4 col-md-6 p-3">
+            <div class="card" style="width:18rem;">
+                <img src="/Images/Poorna.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Poorna Sandukiran</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">CODSE211F-010</h6>
+                    <p class="card-text">
+                        Product Page UI With Backend Implementation
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-lg-4 col-md-6 p-3">
+            <div class="card" style="width:18rem;">
+                <img src="/Images/Sanuja.jpeg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Thiromi Sanuja</h5>
+                    <h6 class="card-subtitle mb-2 text-muted ">CODSE211F-046</h6>
+                    <p class="card-text">
+                        Product Card UI With Backend Implementation
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row m-3">
+        <div class="h3">Contact Us</div>
+        <div class="underline bg-warning mx-3" style="height: 4px; width:2rem"></div>
+    </div>
+    <ul class="list-group m-3 w-50">
+        <li class="list-group-item">
+            <div class="h6"><i class="fa fa-phone" aria-hidden="true"></i>&nbsp;&nbsp;Phone Number</div>
+            <p>+94776675448</p>
+        </li>
+        <li class="list-group-item">
+            <div class="h6"><i class="fa fa-linkedin" aria-hidden="true"></i>&nbsp;&nbsp;Linked In</div>
+            <p><a href="https://www.linkedin.com/in/abdul-muksith-29285a207">Click to View</a></p>
+        </li>
+
+    </ul>
+</body>
+
+</html>
